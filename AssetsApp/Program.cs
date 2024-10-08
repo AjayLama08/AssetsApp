e2fa76e9f9fcfed9aa23ed9513bf0718d5cf6825
AJAY LAMA <Q2043246@live.tees.ac.uk>
2024-10-08 20:35:44 +0100

    Add code to update database record 7

--- AssetsApp/Program.cs
+++ AssetsApp/Program.cs
@@ -18,3 +18,10 @@ var newAsset = new Asset(0, "Tesla Car", Location.KATHMANDU, DateTime.Now, true,
 
 db.Assets.Add(newAsset);
 db.SaveChanges();
+
+var asset = db.Assets.Where(a => a.Id == 7).SingleOrDefault();
+Console.WriteLine($"Before change: {asset}");
+asset.PurchasePrice = 1000;
+Console.WriteLine($"After change: {asset}");
+db.SaveChanges();
+
